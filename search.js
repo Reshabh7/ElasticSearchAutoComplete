5a359b146fa37ca5122743dae6568b109d18d346
Swadesh Behera <itswadesh@gmail.com>
2018-12-06 07:40:18 +0530

    -

--- search.js
+++ search.js
@@ -6,10 +6,8 @@ let elasticsearch = require('elasticsearch');
 let app = express();
 app.use(express.static('static_files'));
 app.use(bodyParser.json());
-
 let mongoose = require("mongoose");
-
-mongoose.connect('mongodb://localhost:27017/Restaurant', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/Restaurant', { useNewUrlParser: true });
 
 let Schema = mongoose.Schema;
 
@@ -78,10 +76,10 @@ app.get("/syncData", function (req, res) {
   stream.on('error', function (err) {
     console.log(err);
   });
-  res.send({result: "successfully indexed Mongo Data to ElasticSearch."});
+  res.send({ result: "successfully indexed Mongo Data to ElasticSearch." });
 });
 
-function queryObject(val,partialQueryString,fuzzyQueryString, filter = []){
+function queryObject(val, partialQueryString, fuzzyQueryString, filter = []) {
   // console.log(partialQueryString);
   // console.log(fuzzyQueryString);
   let queryString = (partialQueryString) ? partialQueryString : fuzzyQueryString;
@@ -89,48 +87,48 @@ function queryObject(val,partialQueryString,fuzzyQueryString, filter = []){
   return {
     "query": {
       "bool":
-        {
-          "must": {
-            "query_string": {
-              "query": `${queryString}`,
-              "default_field": "name",
-              "fuzziness": 1
+      {
+        "must": {
+          "query_string": {
+            "query": `${queryString}`,
+            "default_field": "name",
+            "fuzziness": 1
+          }
+        },
+        "should": [
+          {
+            "span_first": {
+              "match": {
+                "span_term": {
+                  "name": { "value": `${val}` }
+                }
+              },
+              "end": 1
             }
           },
-          "should": [
-            {
-              "span_first": {
-                "match": {
-                  "span_term": {
-                    "name": {"value": `${val}`}
-                  }
-                },
-                "end": 1
-              }
-            },
-            {
-              "span_first": {
-                "match": {
-                  "span_term": {
-                    "name": {"value": `${val}`}
-                  }
-                },
-                "end": 2
-              }
-            },
-            {
-              "span_first": {
-                "match": {
-                  "span_term": {
-                    "name": {"value": `${val}`}
-                  }
-                },
-                "end": 3
-              }
+          {
+            "span_first": {
+              "match": {
+                "span_term": {
+                  "name": { "value": `${val}` }
+                }
+              },
+              "end": 2
             }
-          ],
-          filter: filter
-        }
+          },
+          {
+            "span_first": {
+              "match": {
+                "span_term": {
+                  "name": { "value": `${val}` }
+                }
+              },
+              "end": 3
+            }
+          }
+        ],
+        filter: filter
+      }
     },
     "size": 100
   }
@@ -140,13 +138,13 @@ app.get("/search/:term", function (req, res) {
   let val = req.params.term;
   let list = [];
   Recipe.esSearch(
-    queryObject(val,`*${val}*`), {hydrate: false}, function (err, results) {
+    queryObject(val, `*${val}*`), { hydrate: false }, function (err, results) {
       results.hits.hits.map((each) => {
         list.push(each._source.name);
       });
       console.log(results.hits.total);
       Recipe.esSearch(
-        queryObject(val,undefined,`${val}~`), {hydrate: false}, function (err, results) {
+        queryObject(val, undefined, `${val}~`), { hydrate: false }, function (err, results) {
           results.hits.hits.map((each) => {
             list.push(each._source.name);
           });
@@ -170,19 +168,19 @@ app.get("/filter/:term", function (req, res) {
   });
   let list = [];
   Recipe.esSearch(
-    queryObject(val,`*${val}*`,undefined,[
-      {terms: {ingredients : options}},
+    queryObject(val, `*${val}*`, undefined, [
+      { terms: { ingredients: options } },
     ]),
-    {hydrate: false},
+    { hydrate: false },
     function (err, results) {
       results.hits.hits.map((each) => {
         list.push(each._source.name);
       });
       console.log(results.hits.total);
       Recipe.esSearch(
-        queryObject(val,undefined,`${val}~`,[
-          {terms: {ingredients : options}},
-        ]), {hydrate: false}, function (err, results) {
+        queryObject(val, undefined, `${val}~`, [
+          { terms: { ingredients: options } },
+        ]), { hydrate: false }, function (err, results) {
           results.hits.hits.map((each) => {
             list.push(each._source.name);
           });
